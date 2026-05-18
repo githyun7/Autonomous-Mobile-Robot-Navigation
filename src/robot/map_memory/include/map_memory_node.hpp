7c08042d3f718854939979ae3c3459f4dd3ed014
Hyunseo Lee <hsdaniel07@gmail.com>
2026-05-19 06:36:27 +0900

    Update

--- src/robot/map_memory/include/map_memory_node.hpp
+++ src/robot/map_memory/include/map_memory_node.hpp
@@ -2,50 +2,52 @@
 #define MAP_MEMORY_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+
 #include "nav_msgs/msg/occupancy_grid.hpp"
 #include "nav_msgs/msg/odometry.hpp"
+
+#include "map_memory_core.hpp"
 
 class MapMemoryNode : public rclcpp::Node {
   public:
     MapMemoryNode();
 
   private:
-    // Subscribers
+    void costmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void timerCallback();
+
+    void initializeGlobalMap();
+    void integrateCostmap();
+    bool worldToGlobalGrid(double world_x, double world_y, int & grid_x, int & grid_y);
+    double getYawFromQuaternion(const geometry_msgs::msg::Quaternion & q);
+    double distanceFromLastUpdate();
+
+    robot::MapMemoryCore map_memory_;
+
     rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_sub_;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
-
-    // Publisher
     rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_pub_;
-
-    // Timer
     rclcpp::TimerBase::SharedPtr timer_;
 
-    // Callback functions
-    void costmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr map);
-    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr odom);
-    void updateMap();
-    void integrateCostmap();
-
-    // Map memory attributes
-    int map_width_;  // Number of cells in x direction
-    int map_height_; // Number of cells in y direction
-    double map_resolution_; // Resolution of the map memory (meters per cell)
-
-    // Variables
-    nav_msgs::msg::OccupancyGrid global_map_;
     nav_msgs::msg::OccupancyGrid latest_costmap_;
-    bool costmap_updated_ = false;
-    bool map_initialized_ = false;
-    bool should_update_map_ = false;
-    int costmap_update_counter_ = 0;
+    nav_msgs::msg::OccupancyGrid global_map_;
+    nav_msgs::msg::Odometry latest_odom_;
 
-    // Robot state
-    double current_x_;
-    double current_y_;
-    double current_theta_;
-    double last_x_;
-    double last_y_;
-    double distance_threshold_;
+    bool has_costmap_;
+    bool has_odom_;
+    bool global_map_initialized_;
+    bool first_update_;
+
+    double last_update_x_;
+    double last_update_y_;
+
+    double map_resolution_;
+    int map_width_;
+    int map_height_;
+    double map_origin_x_;
+    double map_origin_y_;
+    double update_distance_threshold_;
 };
 
 #endif
